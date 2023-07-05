9670224723623796219295ad75be9b98f06db337
Abdmaluk <137059149+Abdmaluk@users.noreply.github.com>
2023-07-05 08:09:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,59 +1,56 @@
-var registrationForm = document.getElementById('registration-form');
-var errorMessage = document.getElementById('error-message');
-var database = firebase.database();
+import { initializeApp } from 'firebase/app';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { getDatabase, ref, set } from 'firebase/database';
 
-
-// Existing code for registration form submission handler
-registrationForm.addEventListener('submit', function(event) {
-  event.preventDefault();
-
-  var emailInput = document.getElementById('email-input');
-  var passwordInput = document.getElementById('password-input');
-
-  var email = emailInput.value;
-  var password = passwordInput.value;
-
-  firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then(function(userCredential) {
-      // Registration successful
-      var user = userCredential.user;
-      console.log('User registered:', user);
-
-      // Store user details in the database
-      var userDetails = {
-        email: user.email,
-        // Add additional user details as needed
-      };
-
-      database.ref('users/' + user.uid).set(userDetails)
-        .then(function() {
-          console.log('User details stored in the database');
-          // You can redirect the user to a different page or perform additional actions here
-        })
-        .catch(function(error) {
-          console.error('Error storing user details:', error);
-          // Handle the error and display an appropriate message to the user
-        });
-    })
-    .catch(function(error) {
-      // Registration failed
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.error('Registration error:', errorCode, errorMessage);
-      // Display the error message to the user
-      errorMessage.textContent = errorMessage;
-    });
-});
-
-
-var firebaseConfig = {
+// Initialize Firebase
+const firebaseConfig = {
   apiKey: "AIzaSyDV3F9KWocJgi1R5t90vb002Gg_bKLQIxU",
   authDomain: "users-1e449.firebaseapp.com",
+  databaseURL: "https://users-1e449-default-rtdb.firebaseio.com",
   projectId: "users-1e449",
   storageBucket: "users-1e449.appspot.com",
   messagingSenderId: "763748549016",
   appId: "1:763748549016:web:b1b27fe5cc2c24a5bfabed",
   measurementId: "G-KW39Y6NBVB"
+  // Add your Firebase configuration here
 };
 
-firebase.initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
+
+const auth = getAuth();
+const database = getDatabase();
+
+const signupForm = document.getElementById('signup-form');
+const errorMessage = document.getElementById('error-message');
+
+signupForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const emailInput = document.getElementById('email-input');
+  const passwordInput = document.getElementById('password-input');
+
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+    console.log('User registered:', user);
+
+    const userDetails = {
+      email: user.email,
+      accountStatus: 'pending',
+      // Add additional user details as needed
+    };
+
+    await set(ref(database, 'users/' + user.uid), userDetails);
+    console.log('User details stored in the database');
+    // You can redirect the user to a different page or perform additional actions here
+  } catch (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.error('Registration error:', errorCode, errorMessage);
+    // Display the error message to the user
+    errorMessage.textContent = errorMessage;
+  }
+});
