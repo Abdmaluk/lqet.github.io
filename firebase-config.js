fb3ad9e5e4cbc374bd3185287c84d500fece1319
Abdmaluk <137059149+Abdmaluk@users.noreply.github.com>
2023-07-05 10:24:48 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,6 @@
-const firebaseConfig = {
+define(function() {
+  const firebaseConfig = {
+    // Your Firebase configuration goes here
   apiKey: "AIzaSyDV3F9KWocJgi1R5t90vb002Gg_bKLQIxU",
   authDomain: "users-1e449.firebaseapp.com",
   databaseURL: "https://users-1e449-default-rtdb.firebaseio.com",
@@ -7,4 +9,7 @@ const firebaseConfig = {
   messagingSenderId: "763748549016",
   appId: "1:763748549016:web:b1b27fe5cc2c24a5bfabed",
   measurementId: "G-KW39Y6NBVB"
-};
+  };
+
+  return firebaseConfig;
+})
