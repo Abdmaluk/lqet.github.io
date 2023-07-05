039e9bc979e97b5f055510b97ab8aad3a4c826cf
Abdmaluk <137059149+Abdmaluk@users.noreply.github.com>
2023-07-05 09:44:27 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDV3F9KWocJgi1R5t90vb002Gg_bKLQIxU",
+  authDomain: "users-1e449.firebaseapp.com",
+  databaseURL: "https://users-1e449-default-rtdb.firebaseio.com",
+  projectId: "users-1e449",
+  storageBucket: "users-1e449.appspot.com",
+  messagingSenderId: "763748549016",
+  appId: "1:763748549016:web:b1b27fe5cc2c24a5bfabed",
+  measurementId: "G-KW39Y6NBVB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
