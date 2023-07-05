5a36e6d74ec77ec609e009c2483ac404fb93df76
Abdmaluk <137059149+Abdmaluk@users.noreply.github.com>
2023-07-05 09:40:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,56 +1,31 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
-import { getDatabase, ref, set } from 'firebase/database';
-
 // Initialize Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyDV3F9KWocJgi1R5t90vb002Gg_bKLQIxU",
-  authDomain: "users-1e449.firebaseapp.com",
-  databaseURL: "https://users-1e449-default-rtdb.firebaseio.com",
-  projectId: "users-1e449",
-  storageBucket: "users-1e449.appspot.com",
-  messagingSenderId: "763748549016",
-  appId: "1:763748549016:web:b1b27fe5cc2c24a5bfabed",
-  measurementId: "G-KW39Y6NBVB"
-  // Add your Firebase configuration here
-};
+firebase.initializeApp(firebaseConfig);
 
-initializeApp(firebaseConfig);
+// Get a reference to the sign-up form and the message div
+const form = document.querySelector('#signup-form');
+const message = document.querySelector('#message');
 
-const auth = getAuth();
-const database = getDatabase(app);
-
-const signupForm = document.getElementById('signup-form');
-const errorMessage = document.getElementById('error-message');
-
-signupForm.addEventListener('submit', async (event) => {
+// Handle form submission
+form.addEventListener('submit', (event) => {
   event.preventDefault();
+  const email = form.email.value;
+  const password = form.password.value;
 
-  const emailInput = document.getElementById('email-input');
-  const passwordInput = document.getElementById('password-input');
+  // Create a new user with Firebase Authentication
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Display success message to the user
+      message.innerHTML = 'Sign-up request submitted. Please wait for approval.';
 
-  const email = emailInput.value;
-  const password = passwordInput.value;
-
-  try {
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-    const user = userCredential.user;
-    console.log('User registered:', user);
-
-    const userDetails = {
-      email: user.email,
-      accountStatus: 'pending',
-      // Add additional user details as needed
-    };
-
-    await set(ref(database, 'users/' + user.uid), userDetails);
-    console.log('User details stored in the database');
-    // You can redirect the user to a different page or perform additional actions here
-  } catch (error) {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.error('Registration error:', errorCode, errorMessage);
-    // Display the error message to the user
-    errorMessage.textContent = errorMessage;
-  }
+      // Create a new document in Firestore for the new user
+      const db = firebase.firestore();
+      db.collection('users').doc(userCredential.user.uid).set({
+        email: userCredential.user.email,
+        status: 'pending'
+      });
+    })
+    .catch((error) => {
+      // Display error message to the user
+      message.innerHTML = error.message;
+    });
 });
