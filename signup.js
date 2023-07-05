7dc89f0adb58bb77c0a117c2b520e13dd4a310f9
Abdmaluk <137059149+Abdmaluk@users.noreply.github.com>
2023-07-05 12:07:23 +0300

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,8 +1,13 @@
 // Initialize Firebase
 firebase.initializeApp({
-	apiKey: "<YOUR_API_KEY>",
-	authDomain: "<YOUR_AUTH_DOMAIN>",
-	projectId: "<YOUR_PROJECT_ID>",
+  apiKey: "AIzaSyDV3F9KWocJgi1R5t90vb002Gg_bKLQIxU",
+  authDomain: "users-1e449.firebaseapp.com",
+  databaseURL: "https://users-1e449-default-rtdb.firebaseio.com",
+  projectId: "users-1e449",
+  storageBucket: "users-1e449.appspot.com",
+  messagingSenderId: "763748549016",
+  appId: "1:763748549016:web:b1b27fe5cc2c24a5bfabed",
+  measurementId: "G-KW39Y6NBVB",
 });
 
 // Get a reference to the Firebase authentication service
